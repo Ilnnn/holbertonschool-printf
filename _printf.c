36210b8e04714cfd983878f4aedf10f65ccfac3b
aBDEL <abdel_azrhar@hotmail.com>
2025-07-22 18:36:53 +0200

    Write _printf function to produce output for %c, %s, and %

--- _printf.c
+++ _printf.c
@@ -1,33 +1,32 @@
-#include <unistd.h>
 #include <stdarg.h>
-#include <stdlib.h>
-
+#include "main.h"
 int _printf(const char *format, ...)
 {
-    char *s;
     int i = 0;
-    char c;
+    int j;
     int count = 0;
     va_list argm;
-    va_start (argm, format);
-
+formats form[] = {
+        {'c', print_char},
+        {'s', print_string},
+        {'%', print_percent},
+    };
+    va_start(argm, format);
+ 
     while (format[i] != '\0')
     {
-        if (format[i] == 'c')
+     if (format[i] == '%')
         {
-            c =va_arg(argm, int);
-           count += write(1,c,1);
-        }
-        else if (format[i] == 's')
+        i++;
+        for (j = 0; form[j].type != '\0'; j++)
         {
-            s = va_arg(argm, char*);
-            count += write(1,s,1);
-        }
-        else if (format[i] == '%')
-        {
-            count += write(1,'%',1);
-        }
+        if (format[i] == form[j].type)
+                {
+                count += form[j].f(argm);
+                break;
+                }
+            }    
+    } 
     }
-va_end(argm);
-return(count);    
-}
+return count;
+}    
