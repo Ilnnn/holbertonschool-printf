d27dfcf77fcd4a780223528dcac8e5fddf2e7823
abdel <abdel_azrhar@hotmail.com>
2025-07-23 17:55:00 +0200

    Add Betty-style documentation to main.h

--- main.h
+++ main.h
@@ -1,17 +1,22 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
-
-
+/**
+ *struct format - Structure to associate a format specifier with a function
+ *@type: Character representing the the format
+ *@f: Pointer to function handling the format
+ */
 typedef struct format
 {
-   char type;
-   int (*f)(va_list); 
-}format_t;
+char type;
+int (*f)(va_list);
+} format_t;
 
 int print_char(va_list argm);
 int print_string(va_list argm);
 int print_percent(va_list argm);
+int handle_spec(va_list args);
+int _putchar(char c);
 
 int _printf(const char *format, ...);
 #endif
