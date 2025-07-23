9c554992b8299bff5be61d5b20f1052f882bdd42
abdel <abdel_azrhar@hotmail.com>
2025-07-24 00:03:01 +0200

    apply Betty style corrections to get_type_func.c

--- get_type_func.c
+++ get_type_func.c
@@ -2,27 +2,33 @@
 #include "main.h"
 #include <stddef.h>
 #include <unistd.h>
-int get_type_func(char c , va_list argm)
+/**
+ * get_type_func - Selects the correct print function based on format specifier
+ * @c: Format specifier character 'c', 's', 'd' etc
+ * @argm: va_list containing arguments passed to _printf
+ * Return: Number of characters printed by the matched function
+ */
+int get_type_func(char c, va_list argm)
 {
 	format_t form[] = {
 	{'c', print_char},
-        {'s', print_string},
-        {'%', print_percent},
-        {'d', handle_spec},
-        {'i', handle_spec},
-        {'\0', NULL}
+	{'s', print_string},
+	{'%', print_percent},
+	{'d', handle_spec},
+	{'i', handle_spec},
+	{'\0', NULL}
 	};
 
 	int j;
 
-	for (j = 0;form[j].type != '\0';j++)
+	for (j = 0; form[j].type != '\0'; j++)
 	{
 	if (c == form[j].type)
 		return (form[j].f(argm));
 	}
-	write (1, "%", 1);
-	write (1, &c, 1);
-	return(2);
+	write(1, "%", 1);
+	write(1, &c, 1);
+	return (2);
 
 }
 
