d0e2ca9f0a06028456e54ea2432b4002ed09ada0
Ilnnn <camoreze@gmail.com>
2025-07-23 23:19:23 +0000

    add of all files to the main + create the .exe

--- main.c
+++ main.c
@@ -1,27 +1,44 @@
+#include <limits.h>
+#include <stdio.h>
 #include "main.h"
 
-int test_print_int(const char *dummy, ...)
-{
-	va_list args;
-	int count;
-
-	va_start(args, dummy);
-	count = handle_spec(args);
-	va_end(args);
-
-	return count;
-}
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
 int main(void)
 {
-	test_print_int("", 12345);
-	_putchar('\n');
+    int len;
+    int len2;
+    unsigned int ui;
+    void *addr;
 
-	test_print_int("", -9876);
-	_putchar('\n');
-
-	test_print_int("", 0);
-	_putchar('\n');
-
-	return 0;
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    ui = (unsigned int)INT_MAX + 1024;
+    addr = (void *)0x7ffe637541f0;
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+    _printf("Unsigned:[%u]\n", ui);
+    printf("Unsigned:[%u]\n", ui);
+    _printf("Unsigned octal:[%o]\n", ui);
+    printf("Unsigned octal:[%o]\n", ui);
+    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    _printf("Address:[%p]\n", addr);
+    printf("Address:[%p]\n", addr);
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
+    _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
+    return (0);
 }
--- main.h
+++ main.h
@@ -1,11 +1,23 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdarg.h>
-#include <unistd.h>
+/**
+ *struct format - Structure to associate a format specifier with a function
+ *@type: Character representing the the format
+ *@f: Pointer to function handling the format
+ */
+typedef struct format
+{
+char type;
+int (*f)(va_list);
+} format_t;
 
-int _putchar(char c);
-int print_int(va_list args);
-int _printf(const char *format, ...);
+int print_char(va_list argm);
+int print_string(va_list argm);
+int print_percent(va_list argm);
 int handle_spec(va_list args);
+int _putchar(char c);
+
+int _printf(const char *format, ...);
+int get_type_func(char c, va_list argm);
 #endif
